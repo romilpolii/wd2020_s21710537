3aa6427711f5f4efaa0593e1faf061b644ffa8ab
romilpolii <romilpolii25@gmail.com>
2020-11-10 17:03:56 +0800

    materi menambahkan javascript di html

---
+++ pertemuan_19/index.js
@@ -0,0 +1,3 @@
+//eksternal JavaScript
+/* Cara kedua untuk menyisipkan kode JavaScript di dalam kode HTML adalah dengan menggunakan eksternal JavaScript. Eksternal JavaScript yaitu membuat file JavaScript yang terpisah dengan file HTML. */
+console.log("Hello People eksternal");
